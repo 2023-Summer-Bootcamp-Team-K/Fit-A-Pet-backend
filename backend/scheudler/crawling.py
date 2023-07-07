128bb596910579caeddb80a5f8cf304a60e9b909
hangyeol <snake011128@naver.com>
2023-07-07 22:57:35 +0900

    [#1]Feat: Libre아이디, 비밀번호 .env로 관리

--- backend/scheudler/crawling.py
+++ backend/scheudler/crawling.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,29 +18,35 @@ def run_libreView_process():
     driver.get('https://www.libreview.com/')
     time.sleep(5)
 
+    # 모달 창 선택
     modal = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'truste-consent-track')))
     time.sleep(1)
 
+    # 동의 버튼 클릭
     consent_button = modal.find_element(By.ID, 'truste-consent-button')
     consent_button.click()
 
+    # 나라 선택 모달 클릭
     open_modal_button = driver.find_element(By.ID, 'country-select')
     open_modal_button.click()
     time.sleep(5)
 
+    # '한국' 선택
     country_select = Select(driver.find_element(By.ID, 'country-select'))
     country_select.select_by_value('KR')
 
+    # 완료 버튼 클릭
     confirm_button = driver.find_element(By.ID, 'submit-button')
     confirm_button.click()
 
     time.sleep(5)
 
+    # 이메일 넣기
     email_input = driver.find_element(By.ID, 'loginForm-email-input')
-    email_input.send_keys('charvetier@gmail.com')
+    email_input.send_keys(os.getenv('LIBRE_ID'))
 
     password_input = driver.find_element(By.ID, 'loginForm-password-input')
-    password_input.send_keys('tifmqpxldp!2')
+    password_input.send_keys(os.getenv('LIBRE_PASSWORD'))
 
     login_button = driver.find_element(By.ID, 'loginForm-submit-button')
     login_button.click()
