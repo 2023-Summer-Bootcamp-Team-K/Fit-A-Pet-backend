b701359d519e030f71f4a443d733622c3a51b6d9
Xoeon <jayy_19@ewhain.net>
2023-07-08 03:43:43 +0900

    [#6]Feat: CSV 파일 read logic 구현 -데이터 불러오는 로직 구현 -JsonResponse, csv directory는 아마도 삭제 예정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from data.views import DataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/data/', DataView.as_view()),
 ]
