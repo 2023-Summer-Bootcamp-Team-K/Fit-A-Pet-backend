ab77507ea676d0588ea1919a41ae9cc64f9cdf16
Xoeon <jayy_19@ewhain.net>
2023-07-08 03:43:43 +0900

    [#6]Feat: CSV 파일 read logic 구현-데이터 불러오는 로직 구현-JsonResponse, csv directory는 아마도 삭제 예정

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from .models import Data
+from django.views import View
+
+from django.http import HttpResponse, JsonResponse
+
+import csv
+
+
+class DataView(View):
+    def get(self, request):
+        data_list = []
+
+        with open('/Users/azurespring/projects/Fit-A-Pet-backend/backend/data/csv/SoobinJung_glucose_2023-6-9 (1).csv') as bloodsugar_csv:
+            reader = csv.reader(bloodsugar_csv)
+
+            for i, row in enumerate(reader):
+                if i >= 3:
+                    device = row[0]
+                    code = row[1]
+                    timestamp = row[2]
+                    record_type = int(row[3])
+                    prev_bloodsugar = int(row[4])
+                    cur_bloodsugar = int(row[5])
+
+                    Data.objects.create(
+                        device=device,
+                        code=code,
+                        timestamp=timestamp,
+                        record_type=record_type,
+                        prev_bloodsugar=prev_bloodsugar,
+                        cur_bloodsugar=cur_bloodsugar
+                    )
+
+                    data = {
+                        'device': device,
+                        'code': code,
+                        'timestamp': timestamp,
+                        'record_type': record_type,
+                        'prev_bloodsugar': prev_bloodsugar,
+                        'cur_bloodsugar': cur_bloodsugar
+                    }
+                    data_list.append(data)
+
+        response_data = {'data list': data_list}
+        json_data = json.dumps(response_data, ensure_ascii=False, indent=4)  # 들여쓰기 적용
+
+        return HttpResponse(json_data, content_type='application/json')
