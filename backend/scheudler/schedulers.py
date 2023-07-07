3b1da1f7557cd3857b6675b3788626dc63fe33a2
hangyeol <snake011128@naver.com>
2023-07-07 22:46:39 +0900

    [#1]Feat: 스케줄러 추가

---
+++ backend/scheudler/schedulers.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from crawling import run_libreView_process
+
+# 스케줄러 신청
+scheduler = BlockingScheduler()
+
+# 스케줄링 설정
+scheduler.add_job(run_libreView_process(), 'interval', hours=8)
+
+try:
+    scheduler.start()
+except KeyboardInterrupt:
+    pass
