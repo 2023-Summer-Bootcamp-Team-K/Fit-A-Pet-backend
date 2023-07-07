f0671bf5f3e80f7584df013c413944be23d8a7d8
hangyeol <snake011128@naver.com>
2023-07-07 22:24:08 +0900

    [#1]Feat: 2차코드 받기 전까지 작성 완료

--- backend/scheudler/crawling.py
+++ backend/scheudler/crawling.py
@@ -1,6 +1,5 @@
 import time
 
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,31 +7,46 @@ from selenium.webdriver.support.ui import Select
 
 from selenium import webdriver
 
-# Selenium 웹 드라이버 설정
-driver = webdriver.Chrome()  # 크롬 드라이버 경로를 지정해주세요
-time.sleep(3)
+def run_libreView_process():
+    # Selenium 웹 드라이버 설정
+    driver = webdriver.Chrome()  # 크롬 드라이버 경로를 지정해주세요
+    time.sleep(3)
 
 
-# 웹 페이지로 이동
-driver.get('https://www.libreview.com/')
-time.sleep(5)
+    # 웹 페이지로 이동
+    driver.get('https://www.libreview.com/')
+    time.sleep(5)
 
-modal = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'truste-consent-track')))
-time.sleep(1)
+    modal = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'truste-consent-track')))
+    time.sleep(1)
 
-consent_button = modal.find_element(By.ID, 'truste-consent-button')
-consent_button.click()
+    consent_button = modal.find_element(By.ID, 'truste-consent-button')
+    consent_button.click()
 
-open_modal_button = driver.find_element(By.ID, 'country-select')
-open_modal_button.click()
-time.sleep(5)
+    open_modal_button = driver.find_element(By.ID, 'country-select')
+    open_modal_button.click()
+    time.sleep(5)
 
-country_select = Select(driver.find_element(By.ID, 'country-select'))
-country_select.select_by_value('KR')
+    country_select = Select(driver.find_element(By.ID, 'country-select'))
+    country_select.select_by_value('KR')
 
-confirm_button = driver.find_element(By.ID, 'submit-button')
-confirm_button.click()
+    confirm_button = driver.find_element(By.ID, 'submit-button')
+    confirm_button.click()
 
-time.sleep(5)
+    time.sleep(5)
 
-driver.quit()
+    email_input = driver.find_element(By.ID, 'loginForm-email-input')
+    email_input.send_keys('charvetier@gmail.com')
+
+    password_input = driver.find_element(By.ID, 'loginForm-password-input')
+    password_input.send_keys('tifmqpxldp!2')
+
+    login_button = driver.find_element(By.ID, 'loginForm-submit-button')
+    login_button.click()
+
+    code_send_button = driver.find_element(By.ID, 'twoFactor-step1-next-button')
+    code_send_button.click()
+
+    time.sleep(10)
+
+    driver.quit()
