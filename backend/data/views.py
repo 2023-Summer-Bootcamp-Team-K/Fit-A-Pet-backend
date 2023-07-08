0c876658c7cb35146fa8f2a05bfdd5c029e45fa9
Xoeon <jayy_19@ewhain.net>
2023-07-08 23:17:29 +0900

    [#3]Feat: View 클래스 제거
    -api 구현에 굳이 필요 없어서 제거

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,41 +1,37 @@
 import json
 
 from .models import Data
-from django.views import View
 
 from django.http import HttpResponse, JsonResponse
 
-class DataView(View):
-    def get(self, request):
-        data_list = []
 
-        # Assuming you have set up the MySQL database connection properly
+def get_data(request):
+    data_list = []
 
-        # Fetch data from the MySQL database
-        queryset = Data.objects.all()
+    queryset = Data.objects.all()
 
-        for data in queryset:
-            # Extract the required fields from the data object
-            device = data.device
-            code = data.code
-            timestamp = data.timestamp.isoformat()
-            record_type = data.record_type
-            bloodsugar = data.bloodsugar
-            scan_bloodsugar = data.scan_bloodsugar
+    for data in queryset:
+        # Extract the required fields from the data object
+        device = data.device
+        code = data.code
+        timestamp = data.timestamp.isoformat()
+        record_type = data.record_type
+        bloodsugar = data.bloodsugar
+        scan_bloodsugar = data.scan_bloodsugar
 
-            # Create a dictionary for the data
-            data = {
-                'device': device,
-                'code': code,
-                'timestamp': timestamp,
-                'record_type': record_type,
-                'bloodsugar': bloodsugar,
-                'scan_bloodsugar': scan_bloodsugar
-            }
+        # Create a dictionary for the data
+        data = {
+            'device': device,
+            'code': code,
+            'timestamp': timestamp,
+            'record_type': record_type,
+            'bloodsugar': bloodsugar,
+            'scan_bloodsugar': scan_bloodsugar
+        }
 
-            data_list.append(data)
+        data_list.append(data)
 
-        response_data = {'data list': data_list}
-        json_data = json.dumps(response_data, ensure_ascii=False, indent=4)  # Apply indentation
+    response_data = {'data list': data_list}
+    json_data = json.dumps(response_data, ensure_ascii=False, indent=4)  # Apply indentation
 
-        return HttpResponse(json_data, content_type='application/json')
+    return HttpResponse(json_data, content_type='application/json')
