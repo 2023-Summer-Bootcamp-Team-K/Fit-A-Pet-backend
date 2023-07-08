25704ad428c2415d7cddc3183eb37f5c786bb111
hangyeol <snake011128@naver.com>
2023-07-08 22:19:21 +0900

    [#1]Fix:스케줄러 수정

--- backend/scheudler/schedulers.py
+++ backend/scheudler/schedulers.py
@@ -7,7 +7,4 @@ scheduler = BlockingScheduler()
 # 스케줄링 설정
 scheduler.add_job(run_libreView_process(), 'interval', hours=8)
 
-try:
-    scheduler.start()
-except KeyboardInterrupt:
-    pass
+scheduler.start()
