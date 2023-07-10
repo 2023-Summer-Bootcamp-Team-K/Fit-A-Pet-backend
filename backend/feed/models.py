f70baa1bab0eeb4f481d77f3593117714af5dcb7
hangyeol <snake011128@naver.com>
2023-07-10 15:55:50 +0900

    [#7]Feat: Meat. Oil, Supplement, MixedFeed 모델 생성

--- backend/feed/models.py
+++ backend/feed/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+from pet.models import Pet
+
+
 # Create your models here.
+class Meat(models.Model):
+    name = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'meat'
+
+
+class Oil(models.Model):
+    name = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'oil'
+
+
+class Supplement(models.Model):
+    name = models.CharField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'supplement'
+
+
+class MixedFeed(models.Model):
+    meat = models.ForeignKey(Meat, on_delete=models.CASCADE)
+    oil = models.ForeignKey(Oil, on_delete=models.CASCADE)
+    supplement = models.ForeignKey(Supplement, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
