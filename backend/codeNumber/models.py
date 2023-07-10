cca4f98155c48cae400c217fe8d16334d6982bff
Xoeon <jayy_19@ewhain.net>
2023-07-10 19:04:15 +0900

    [#3]Feat: device_num max_length 수정

--- backend/codeNumber/models.py
+++ backend/codeNumber/models.py
@@ -5,8 +5,9 @@ from data.models import Data
 
 # Create your models here.
 class codeNumber(models.Model):
+
     pet_id = models.ForeignKey(Pet, on_delete=models.CASCADE)
-    device_num = models.ForeignKey(Data, on_delete=models.CASCADE)
+    device_num = models.CharField(max_length=50)
 
     class Meta:
         db_table = 'code'
