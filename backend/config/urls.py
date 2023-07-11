42c47e5e35f89cebc48e5e285688101dd28083fc
Xoeon <jayy_19@ewhain.net>
2023-07-12 01:54:07 +0900

    [#9]Feat: 스웨거 연동 url 수정
    -main에서 판 브랜치 코드 옮겨오다가 api/pets 부분 잘못 가져온 것도 고쳤습니다

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -10,18 +12,17 @@ schema_view = get_schema_view(
         default_version='v1',
         description="반려동물을 위한 맞춤 사료 추천",
     ),
-    validators=['flex'],
     public=True,
-    permission_classes=(AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     # swagger
-    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     # django
     path('admin/', admin.site.urls),
-    path('api/pets/', include('pet.urls')),
+    path('api/data/', include('data.urls')),
 ]
