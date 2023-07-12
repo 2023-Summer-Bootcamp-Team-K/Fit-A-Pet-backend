317661ffa0d4730f843dce5362f87d3cf66bd881
limjihoon99 <bo9701@naver.com>
2023-07-12 21:24:22 +0900

    [#4]Feat: s3 이미지 수정 구현

--- backend/pet/views.py
+++ backend/pet/views.py
@@ -20,7 +20,6 @@ class PetCreateView(APIView):
             if 'profile_image' in request.FILES:
                 image = request.FILES['profile_image']
 
-
                 s3_file_path = f"fitapet/{image.name}"
 
                 pet.profile_url = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/{s3_file_path}"
@@ -56,18 +55,56 @@ class PetCreateView(APIView):
 
 
 class PetModifyView(APIView):
+    parser_classes = [parsers.MultiPartParser, parsers.FormParser]
+
     def patch(self, request, pet_id):
         try:
             pet = Pet.objects.get(id=pet_id)
         except Pet.DoesNotExist:
-            return Response({"message": "Pet not found"}, status=status.HTTP_404_NOT_FOUND)
+            response_data = {
+                'code': status.HTTP_404_NOT_FOUND,
+                'is_success': False,
+                'message': '해당 반려동물을 찾을 수 없습니다',
+            }
+            return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
         serializer = PetSerializer(pet, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            pet = serializer.save()
+            if 'profile_image' in request.FILES:
+                image = request.FILES['profile_image']
+
+                s3_file_path = f"fitapet/{image.name}"
+
+                pet.profile_url = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com/{s3_file_path}"
+                pet.save()
+
+            response_data = {
+                'code': status.HTTP_200_OK,
+                'is_success': True,
+                'message': '반려동물 정보가 성공적으로 수정되었습니다',
+                'result': {
+                    'id': pet.id,
+                    'user': pet.user_id,
+                    'name': pet.name,
+                    'age': pet.age,
+                    'species': pet.species,
+                    'gender': pet.gender,
+                    'weight': pet.weight,
+                    'started_date': pet.started_date,
+                    'feed': pet.feed,
+                    'sore_spot': pet.sore_spot,
+                    'profile_url': pet.profile_url,
+                }
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            response_data = {
+                'code': status.HTTP_400_BAD_REQUEST,
+                'is_success': False,
+                'message': '유효하지 않은 데이터입니다',
+            }
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PetDeleteView(APIView):
