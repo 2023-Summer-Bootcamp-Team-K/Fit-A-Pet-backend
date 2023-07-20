896f66ac5859e2f579cbafc9dba505251afdc75e
hangyeol <snake011128@naver.com>
2023-07-20 15:09:04 +0900

    [#31]Fix: one_day 함수 수정

--- backend/data/views.py
+++ backend/data/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -38,27 +40,35 @@ def get_data(request, pet_id):
 
 @api_view(['GET'])
 def get_one_day_data(request, pet_id):
-    one_day_data_list = []
+    data_list = []
     code_number = codeNumber.objects.get(pet_id=pet_id)
-    one_day_queryset = Data.objects.filter(code=code_number.device_num)
+    queryset = Data.objects.filter(code=code_number.device_num).order_by('timestamp')  # 날짜 기준으로 정렬
 
-    user_datetime = dt.datetime(2023, 6, 3, 0, 0, 0, tzinfo=timezone.get_current_timezone())
+    current_date = None
+    current_data = None
 
-    one_day_ago = user_datetime - timedelta(days=1)
-    one_day_data = one_day_queryset.filter(timestamp__gte=one_day_ago, timestamp__lte=user_datetime)
+    for data in queryset:
+        timestamp = data.timestamp
+        date = timestamp.astimezone(timezone.get_current_timezone()).date()
+        time = timestamp.astimezone(timezone.get_current_timezone()).time()
 
-    for data in one_day_data:
-        device = data.device
-        code = codeNumber.device_num
-        timestamp = data.timestamp.isoformat()
-        record_type = data.record_type
-        bloodsugar = data.bloodsugar
-        scan_bloodsugar = data.scan_bloodsugar
+        if current_date is None:
+            current_date = date
+            current_data = {'date': date.isoformat(), 'data': []}
+
+        if current_date != date:
+            if current_data['data']:
+                data_list.append(current_data)
+            current_date = date
+            current_data = {'date': date.isoformat(), 'data': []}
 
         serializer = ChartSerializer(data)
-        one_day_data_list.append(serializer.data)
+        current_data['data'].append(serializer.data)
 
-    response_data = {'time_range_data_list': one_day_data_list}
+    if current_data['data']:
+        data_list.append(current_data)
+
+    response_data = {'data_list': data_list}
     return Response(response_data)
 
 
