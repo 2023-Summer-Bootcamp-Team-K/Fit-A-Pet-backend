553981161029139491d71c914136a24d666c758e
hangyeol <snake011128@naver.com>
2023-07-30 00:39:58 +0900

    [#55]Refactor: suggestion 폴더 리팩토링

--- backend/suggestion/models.py
+++ backend/suggestion/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Suggestion(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     contents = models.CharField(max_length=2000)
