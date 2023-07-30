ef2d7159718c39fe5658293323effad20b777e56
Xoeon <jayy_19@ewhain.net>
2023-07-31 02:06:56 +0900

    [#57]Test: CalCulateHba1cTestCase 추가

--- backend/data/tests.py
+++ backend/data/tests.py
@@ -1,2 +1,68 @@
-from django.test import TestCase
+import datetime
+import json
 
+from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework.test import APIClient
+
+from .views import calculate_hba1c
+from codeNumber.models import codeNumber
+from pet.models import Pet
+from .models import Data
+
+class CalculateHba1cTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user = User.objects.create_user(username="test_user", email="test@pet.com", password="123")
+
+        self.pet = Pet.objects.create(id=10, name="Test Pet", feed="닭고기 사료", age=2, sore_spot="관절", weight=4,
+                                      started_date=timezone.make_aware(datetime.datetime(2023, 7, 12, 0, 0, 0)),
+                                      user=self.user)
+
+        # Create a codeNumber object with the specific Pet instance and device_num
+        self.device_num = "ABC123"
+        self.code_number = codeNumber.objects.create(pet_id=self.pet, device_num=self.device_num)
+
+        self.factory = RequestFactory()
+
+
+    def test_calculate_hba1c(self):
+        # Create a sample Data object for the specific pet_id
+        timestamp = timezone.make_aware(datetime.datetime(2023, 7, 30, 12, 0, 0))
+        data = Data.objects.create(code=self.device_num, bloodsugar=100, timestamp=timestamp, record_type=1) # Provide a value for the 'record_type' field
+
+        # Create a request object for the view function
+        url = reverse('calculate-hba1c', kwargs={'pet_id': self.pet.id})
+        request = self.factory.get(url)
+
+        # Get the response from the view function
+        response = calculate_hba1c(request, pet_id=self.pet.id)
+
+        # Assert that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Assert the calculated HbA1c value in the response data
+        expected_hba1c = (100 + 46.7) / 28.7
+        expected_hba1c_rounded = round(expected_hba1c, 1)
+
+        # Access JSON data through response.json attribute
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data['hba1c'], expected_hba1c_rounded)
+
+    def test_calculate_hba1c_pet_not_found(self):
+        # Test when the specified pet_id does not exist
+        invalid_pet_id = self.pet.id + 1
+        url = reverse('calculate-hba1c', kwargs={'pet_id': invalid_pet_id})
+        request = self.factory.get(url)
+
+        response = calculate_hba1c(request, pet_id=invalid_pet_id)
+
+        # Assert that the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, 404)
+
+        # Access JSON data through response.json attribute
+        response_data = json.loads(response.content)
+        self.assertEqual(response_data['message'], 'codeNumber data not found for the specified pet_id')
--- backend/data/urls.py
+++ backend/data/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hba1c/<int:pet_id>/', views.calculate_hba1c),
+    path('hba1c/<int:pet_id>/', views.calculate_hba1c, name='calculate-hba1c'),
     path('recent/<int:pet_id>/', views.get_most_recent_data),
     path('<int:month>-<int:day>/<int:pet_id>/', views.get_one_day_data),
     path('<int:start_month>-<int:start_day>/<int:end_month>-<int:end_day>/<int:pet_id>/', views.get_interval_data),
