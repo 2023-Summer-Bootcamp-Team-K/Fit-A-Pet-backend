b45b82fe1ba6ccdf1723b281cfcc38f2f1acc7be
limjihoon99 <bo9701@naver.com>
2023-07-31 00:36:53 +0900

    [#59]Feat: pet 테스트 코드 작성

--- backend/pet/tests.py
+++ backend/pet/tests.py
@@ -1,3 +1,141 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Pet
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+import traceback
+
+
+class PetAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_pet(self):
+        url = reverse('pet:pet_create', kwargs={'user_id': self.user.id})
+        data = {
+            "name": "Test",
+            "age": 10,
+            "species": "푸들",
+            "gender": "수컷",
+            "weight": 2.7,
+            "started_date": "2023-07-30",
+            "feed": "돼지고기 사료",
+            "sore_spot": "눈",
+            "profile_url": "https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg",
+        }
+
+        response = self.client.post(url, data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        print("테스트 성공")
+
+    def test_modify_pet(self):
+        pet = Pet.objects.create(
+            user=self.user,
+            name="Test",
+            age=10,
+            species="푸들",
+            gender="수컷",
+            weight=2.7,
+            started_date=timezone.make_aware(timezone.datetime(2023, 7, 30)),
+            feed="돼지고기 사료",
+            sore_spot="눈",
+            profile_url="https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg"
+        )
+
+        url = reverse('pet:pet_modify', kwargs={'pet_id': pet.id})
+        data = {
+            "name": "Mod Test",
+            "age": 12,
+            "species": "골든리트리버",
+            "gender": "암컷",
+            "weight": 2.8,
+            "started_date": "2023-07-31T00:00:00Z",
+            "feed": "소고기 사료",
+            "sore_spot": "기관지",
+            "profile_url": "https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg"
+        }
+
+        response = self.client.patch(url, data=data)
+        if response.status_code != status.HTTP_200_OK:
+            try:
+                response.raise_for_status()  # HTTP 에러 체크
+                response_data = response.json()  # JSON 데이터로 변환
+                print(response_data['message'])  # 에러 메시지 출력
+            except Exception as e:
+                print("에러 발생:")
+                traceback.print_exc()  # 에러의 트레이스백 출력
+                return
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['result']['name'], "Mod Test")  # 'result'에서 'name'을 찾음
+
+        print("테스트 성공")
+
+    def test_list_pets(self):
+        Pet.objects.create(
+            user=self.user,
+            name="Test",
+            age=10,
+            species="푸들",
+            gender="수컷",
+            weight=2.7,
+            started_date="2023-07-30",
+            feed="돼지고기 사료",
+            sore_spot="눈",
+            profile_url="https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg"
+        )
+
+        url = reverse('pet:pet_list', kwargs={'user_id': self.user.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+        print("테스트 성공")
+
+    def test_retrieve_pet(self):
+        pet = Pet.objects.create(
+            user=self.user,
+            name="Test",
+            age=10,
+            species="푸들",
+            gender="수컷",
+            weight=2.7,
+            started_date=timezone.datetime(2023, 7, 30),
+            feed="돼지고기 사료",
+            sore_spot="눈",
+            profile_url="https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg"
+        )
+
+        url = reverse('pet:pet_detail', kwargs={'pet_id': pet.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], "Test")
+
+        print("테스트 성공")
+
+    def test_delete_pet(self):
+        pet = Pet.objects.create(
+            user=self.user,
+            name="Test",
+            age=10,
+            species="푸들",
+            gender="수컷",
+            weight=2.7,
+            started_date="2023-07-30",
+            feed="돼지고기 사료",
+            sore_spot="눈",
+            profile_url="https://fit-a-pet-bucket.s3.ap-northeast-2.amazonaws.com/fitapet/dog.jpeg"
+        )
+
+        url = reverse('pet:pet_delete', kwargs={'pet_id': pet.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Pet.objects.filter(id=pet.id).exists())  # 수정된 부분
+
+        print("테스트 성공")
