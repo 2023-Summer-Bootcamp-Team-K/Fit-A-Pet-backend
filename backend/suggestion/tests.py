29b182c05783553585617b404ad619fed8233677
limjihoon99 <bo9701@naver.com>
2023-07-31 01:36:08 +0900

    [#60]Feat: suggestion 주석 제거

--- backend/suggestion/tests.py
+++ backend/suggestion/tests.py
@@ -8,18 +8,15 @@ from .models import Suggestion
 
 class SuggestionAPITest(TestCase):
     def setUp(self):
-        # 테스트를 위해 유저 생성
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
     def test_create_suggestion(self):
-        # 이용자 건의사항 생성 API 테스트
         url = reverse('suggestion:suggestions', args=[self.user.id])
         data = {"contents": "우리집 강아지 살려주세요"}
         response = self.client.post(url, data, format='json')
 
-        # 성공적으로 생성되었는지 확인
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Suggestion.objects.count(), 1)
         suggestion = Suggestion.objects.first()
@@ -27,11 +24,9 @@ class SuggestionAPITest(TestCase):
         self.assertEqual(suggestion.contents, "우리집 강아지 살려주세요")
 
     def test_create_suggestion_missing_contents(self):
-        # 필수 값 누락으로 인한 실패 테스트
         url = reverse('suggestion:suggestions', args=[self.user.id])
-        data = {}  # contents 필드가 누락됨
+        data = {}
         response = self.client.post(url, data, format='json')
 
-        # 필수 값 누락으로 인한 400 에러 확인
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Suggestion.objects.count(), 0)
