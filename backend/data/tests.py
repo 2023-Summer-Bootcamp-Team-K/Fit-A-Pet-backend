93e1413d8c038d6b18849e1251a3e2d644072456
Xoeon <jayy_19@ewhain.net>
2023-07-31 18:08:22 +0900

    [#57]Test: CalculateHba1cTestCase 코드 리팩토링
    - def setUP 수정
    - 이에 따른 test 함수 수정

--- backend/data/tests.py
+++ backend/data/tests.py
@@ -7,7 +7,8 @@ from django.urls import reverse
 from django.utils import timezone
 from rest_framework.test import APIClient
 
-from .views import calculate_hba1c
+from .views import calculate_hba1c, get_most_recent_data, get_one_day_data, get_interval_data, get_month_data
+
 from codeNumber.models import codeNumber
 from pet.models import Pet
 from .models import Data
@@ -16,74 +17,82 @@ class CalculateHba1cTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-        self.user = User.objects.create_user(username="test_user", email="test@pet.com", password="123")
+        self.user = User.objects.create_user(
+            username="test_user",
+            email="test@pet.com",
+            password="123"
+        )
 
-        self.pet = Pet.objects.create(id=10, name="Test Pet", feed="닭고기 사료", age=2, sore_spot="관절", weight=4,
-                                      started_date=timezone.make_aware(datetime.datetime(2023, 7, 12, 0, 0, 0)),
-                                      user=self.user)
+        self.pet = Pet.objects.create(
+            id=10,
+            name="Test Pet",
+            feed="닭고기 사료",
+            age=2,
+            sore_spot="관절",
+            weight=4,
+            started_date=timezone.make_aware(datetime.datetime(2023, 7, 1, 0, 0, 0)),
+            user=self.user
+        )
 
-        # Create a codeNumber object with the specific Pet instance and device_num
-        self.device_num = "ABC123"
-        self.code_number = codeNumber.objects.create(pet_id=self.pet, device_num=self.device_num)
+        self.data = Data.objects.create(
+            device="FreeStyle LibreLink",
+            code="000A0A00-0AAA-00A0-A00A-000000AA000A",
+            timestamp=datetime.datetime(2023, 7, 30, 12, 0, 0),
+            record_type=0,
+            bloodsugar=100,
+            created_at=timezone.make_aware(datetime.datetime(2023, 7, 1, 0, 0, 0)),
+            updated_at=timezone.make_aware(datetime.datetime(2023, 7, 2, 0, 0, 0))
+        )
+
+        self.codenumber = codeNumber.objects.create(
+            pet_id=self.pet,
+            device_num="000A0A00-0AAA-00A0-A00A-000000AA000A"
+        )
 
         self.factory = RequestFactory()
 
 
     def test_calculate_hba1c(self):
-        # Create a sample Data object for the specific pet_id
-        timestamp = timezone.make_aware(datetime.datetime(2023, 7, 30, 12, 0, 0))
-        data = Data.objects.create(code=self.device_num, bloodsugar=100, timestamp=timestamp, record_type=1) # Provide a value for the 'record_type' field
-
-        # Create a request object for the view function
         url = reverse('calculate-hba1c', kwargs={'pet_id': self.pet.id})
         request = self.factory.get(url)
 
-        # Get the response from the view function
         response = calculate_hba1c(request, pet_id=self.pet.id)
 
-        # Assert that the response status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
 
-        # Assert the calculated HbA1c value in the response data
-        expected_hba1c = (100 + 46.7) / 28.7
+        expected_hba1c = (self.data.bloodsugar + 46.7) / 28.7
         expected_hba1c_rounded = round(expected_hba1c, 1)
 
-        # Access JSON data through response.json attribute
         response_data = json.loads(response.content)
         self.assertEqual(response_data['hba1c'], expected_hba1c_rounded)
 
-    def test_calculate_hba1c_empty_data(self):
-        # Call the view with a pet_id that has no associated Data objects
-        pet_id = self.pet.id
-        url = reverse('calculate-hba1c', args=[pet_id])
-        request = self.factory.get(url)
-        response = calculate_hba1c(request, pet_id)
+        print("테스트 성공")
 
-        # Check if the response status code is 200
+    def test_calculate_hba1c_empty_data(self):
+        url = reverse('calculate-hba1c', kwargs={'pet_id': self.pet.id})
+        request = self.factory.get(url)
+        response = calculate_hba1c(request, self.pet.id)
+
         self.assertEqual(response.status_code, 200)
 
-        # Parse the response content as JSON
+        expected_hba1c = (self.data.bloodsugar + 46.7) / 28.7
+        expected_hba1c_rounded = round(expected_hba1c, 1)
+
         response_data = json.loads(response.content)
+        self.assertEqual(response_data['hba1c'], expected_hba1c_rounded)
 
-        # Ensure that the 'hba1c' field is present in the response_data
-        self.assertIn('hba1c', response_data)
-
-        # Compare the 'hba1c' value with the expected value
-        expected_hba1c = None
-        self.assertEqual(response_data['hba1c'], expected_hba1c)
+        print("테스트 성공")
 
     def test_calculate_hba1c_pet_not_found(self):
-        # Test when the specified pet_id does not exist
         invalid_pet_id = self.pet.id + 1
         url = reverse('calculate-hba1c', kwargs={'pet_id': invalid_pet_id})
         request = self.factory.get(url)
 
         response = calculate_hba1c(request, pet_id=invalid_pet_id)
 
-        # Assert that the response status code is 404 (Not Found)
         self.assertEqual(response.status_code, 404)
 
-        # Access JSON data through response.json attribute
         response_data = json.loads(response.content)
         self.assertEqual(response_data['message'], 'codeNumber data not found for the specified pet_id')
 
+        print("테스트 성공")
